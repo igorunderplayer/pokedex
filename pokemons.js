f6f82331059c3e86bcbd36ebd63936e40c11e550
igorunderplayer <igorunderplayer>
2024-06-11 20:20:01 -0300

    feat: Adicionar modal com informações sobre o Pokemon

--- pokemons.js
+++ pokemons.js
@@ -1,4 +1,6 @@
-const API_POKEMONS = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0";
+const POKE_QUANTIDADE = 20
+
+const API_POKEMONS = `https://pokeapi.co/api/v2/pokemon/?limit=${POKE_QUANTIDADE}&offset=0`;
 const API_IMAGENS = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 let apiPaginaProximo = "";
@@ -6,6 +8,8 @@ let apiPaginaAnterior = "";
 
 let pokemons = [];
 
+
+// Elemento de carregamento "loading"
 const loading = document.createElement("div");
 loading.classList.add("loader");
 
@@ -14,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnProximo = document.getElementById("btnProximo");
     const btnAnterior = document.getElementById("btnAnterior");
     const caixaPokemons = document.getElementById("caixaPokemons");
+    const modalContainer = document.getElementById("modalContainer");
 
     buscarPokemons(API_POKEMONS);
 
@@ -25,22 +30,34 @@ document.addEventListener("DOMContentLoaded", () => {
         if (apiPaginaAnterior) buscarPokemons(apiPaginaAnterior);
     });
 
+    document.addEventListener("click", (event) => {
+        if (event.target == modalContainer || event.target.id == "fecharModal") {
+            modalContainer.style.display = "none";
+        }
+    });
+
     function buscarPokemons(url) {
         pokemons = [];
         caixaPokemons.innerHTML = "";
         caixaPokemons.append(loading);
 
-        fetch(url, { headers: { "Accept": "*" } })
+        fetch(url, { headers: { "Accept": "application/json" } })
             .then(res => res.json())
             .then(data => {
-                apiPaginaAnterior = data.previous;
-                apiPaginaProximo = data.next;
-                pokemons = data.results;
+                apiPaginaAnterior = data.previous ?? "";
+                apiPaginaProximo = data.next ?? "";
+                pokemons = data.results ?? "";
 
                 caixaPokemons.innerHTML = "";
+
+                // Desabilita os botoes que não tiver um url disponível
+                btnProximo.disabled = !apiPaginaProximo;
+                btnAnterior.disabled = !apiPaginaAnterior;
+
                 pokemons.forEach(pokemon => {
                     // Extrai apenas o ID do pokemon para obter sprite do pokemon (remove link da api original e remove o / no final)
-                    const urlImagemPokemon = API_IMAGENS + pokemon.url.split("pokemon")[1].slice(0, -1) + ".png";
+                    const pokemonId = pokemon.url.split("pokemon")[1].slice(0, -1);
+                    const urlImagemPokemon = API_IMAGENS + pokemonId + ".png";
 
                     const containerPokemon = document.createElement("div");
                     const imagemPokemon = document.createElement("img");
@@ -58,9 +75,27 @@ document.addEventListener("DOMContentLoaded", () => {
                     containerPokemon.append(imagemPokemon);
                     caixaPokemons.append(containerPokemon);
 
-                    btnProximo.disabled = !apiPaginaProximo;
-                    btnAnterior.disabled = !apiPaginaAnterior;
+                    containerPokemon.addEventListener('click', () => {
+                        modalContainer.style.display = "flex";
+                        const elementoNomePokemon = document.getElementById("modalPokemonNome");
+                        const elementoImagemPokemon = document.getElementById("modalPokemonImagem");
+                        const elementoPokemonStats = document.getElementById("pokemonStats");
 
+                        elementoPokemonStats.innerHTML = "";
+                        elementoNomePokemon.innerText = pokemon.name;
+                        elementoImagemPokemon.src = urlImagemPokemon;
+
+                        fetch(pokemon.url, { headers: { "Accept": "application/json" } })
+                            .then(res => res.json())
+                            .then(dadosPokemon => {
+                                dadosPokemon.stats.forEach(stat => {
+                                    const spanStatus = document.createElement('span');
+                                    spanStatus.innerText = `${stat.stat.name}: ${stat.base_stat}`;
+
+                                    elementoPokemonStats.append(spanStatus);
+                                })
+                            })
+                    })
                 });
             });
     }
